d9114427a242aec3982e2b74aebd9134d7ec9b73
Bruno Cocco <brunoe.cocco@gmail.com>
2025-11-29 14:06:16 +0100

    sumando practica

--- src/components/Tarjetas.jsx
+++ src/components/Tarjetas.jsx
@@ -1,26 +1,50 @@
 import { useEffect, useState } from "react";
 
-function Tarjetas() {
-  const [datos, setDatos] = useState([]);
+ function Tarjetas() {
+    const [img , setImg] = useState("")
+    
+    const api = async ()=>{
+       const res = await fetch("https://dog.ceo/api/breeds/image/random")
+        const data = await res.json();
+        setImg(data.message)
+      }
+      useEffect(()=>{api()},[])
 
-  const cargarTarjetas = async () => {
-    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=50");
-    const data = await res.json();
-    setDatos(data.results);
-    console.log(datos);
-  };
-
-useEffect(() => {
-  cargarTarjetas();
-}, []);
-
-  return (
+  return(
     <>
-      {datos.map((card, i) => (
-        <p key={i}>{card.name}</p>
-      ))}
-    </>
-  );
-}
+    <section>
+      <article>
+       <img src={img} />
+       <button onClick={api} >apreta gil</button>
+      </article>
+    </section>
 
+    </>
+
+);
+}
 export default Tarjetas;
+//   const [datos, setDatos] = useState([]);
+
+//   const cargarTarjetas = async () => {
+//     const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=50");
+//     const data = await res.json();
+//     setDatos(data.results);
+//     console.log(datos);
+//   };
+
+//   useEffect(() => {
+//     cargarTarjetas();
+//   }, []);
+
+  
+//   return (
+//     <>
+//       <section>
+//         <article>
+//           {datos.map((card, i) => (
+//             <p key={i}>{card.name}</p>
+//           ))}
+//         </article>
+//       </section>
+//     </>
