783f0abc50c1b51c8a0b6fb029a7a16598b6ebe0
Bruno Cocco <brunoe.cocco@gmail.com>
2025-11-30 17:42:04 +0100

    seguimos

---
+++ src/practicaNode/prac3.js
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import readline from "readline";
+
+function practica3() {
+  const archivo = process.argv[2];
+
+  const stream = fs.createReadStream(archivo, 'utf-8');
+ 
+  const rl = readline.createInterface({
+    input: stream,
+  });
+  let contadorLineas = 0;
+
+  rl.on("line", () => {
+    contadorLineas++;
+  });
+
+  rl.on("close", () => {
+    console.log("Total de líneas:", contadorLineas);
+  });
+
+  stream.on("data", (chunk) => {
+    console.log("Chunk:", chunk.length, "bytes");
+  });
+}
+practica3();
